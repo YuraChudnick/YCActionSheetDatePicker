4124c52e6161508764c8f5a2070bb7195b4559e3
Y.Chudnick <y.chudnovets@temabit.com>
2019-01-10 13:17:00 +0200

    Initial Commit

---
+++ YCActionSheetDatePicker/YCActionSheetDatePicker.h
@@ -0,0 +1,19 @@
+//
+//  YCActionSheetDatePicker.h
+//  YCActionSheetDatePicker
+//
+//  Created by Yurii Chudnovets on 1/10/19.
+//  Copyright © 2019 Yurii Chudnovets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YCActionSheetDatePicker.
+FOUNDATION_EXPORT double YCActionSheetDatePickerVersionNumber;
+
+//! Project version string for YCActionSheetDatePicker.
+FOUNDATION_EXPORT const unsigned char YCActionSheetDatePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YCActionSheetDatePicker/PublicHeader.h>
+
+
